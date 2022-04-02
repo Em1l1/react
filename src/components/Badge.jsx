48cc752540eb4684b578e2b0ed0107739e010368
Emily <brancocronos@hotmail.com>
2022-04-02 17:48:30 -0400

    avatar

---
+++ src/components/Badge.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import "./styles/Badge.css"
+import confLogo from '../images/badge-header.svg'
+
+
+class Badge extends React.Component {
+  render () {
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
+
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+          <h1>Victor <br/> Juarez</h1>
+        </div>
+
+        <div className="Badge__section-info">
+          <h3>Frontend Engineer</h3>
+          <div>@victorgame_</div>
+        </div>
+
+        <div className="Badge__footer">
+          #platziconf
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-// //
-// const container = document.getElementById('app');
-// //
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const name = 'Platzi';
+import './global.css';
+import Badge from './components/Badge';
+
+// const name = 'Platzi';
 // const element = <h1>Hello, Platzi Badges!</h1>;
 // const jsx = <h1>Hello, {name} Badges!</h1>
-const element = React.createElement('a', { href: 'https://platzi.com'}, `Ir a ${name}...`)
+// const element = React.createElement('a', { href: 'https://platzi.com'}, `Ir a ${name}...`)
 
-const container = document.getElementById('app');
+// const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+const container = document.getElementById('app');
+ReactDOM.render(<Badge />, container);
