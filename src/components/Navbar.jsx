ca8e0e68a2b0174a65746726c33da032d79c730f
Emily <brancocronos@hotmail.com>
2022-04-26 08:10:05 -0400

    header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+
+import './styles/Navbar.css';
 import logo from '../images/logo.svg';
 
 class Navbar extends React.Component {
   render () {
     return(
-      <div>
-        <a href="/">
-          <img src={logo} alt="Logo" />
-          <span className="font-weight-light">Platzi</span>
-          <span className="font-weight-bold">Conf</span>
-        </a>
+      <div className="Navbar">
+        <div className="container-fluid">
+          <a className="Navbar__brand" href="/">
+            <img className="Navbar__brand-logo" src={logo} alt="Logo" />
+            <span className="font-weight-light">Platzi</span>
+            <span className="font-weight-bold">Conf</span>
+          </a>
+        </div>
+      
       </div>
     );
   }
--- src/pages/BadgeNew.jsx
+++ src/pages/BadgeNew.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg'
 import Navbar from '../components/Navbar';
+import Badge from '../components/Badge';
+
 
 class BadgeNew extends React.Component {
   render() {
     return (
       <div>
         <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge firstName="Victor" lastName="Juarez" twitter="sparragus" jobTitle="Frontend Engineer" avatarUrl="https://www.gravatar.com/avatar?d=identicon" />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
