16d91c4c00c456d81ad1ad11cce2998a3bae2840
Victoriano <mytacam78@gmail.com>
2022-05-14 00:24:06 -0600

    fixed(button) New Page NotFound and redirection Home Co-authored-by @Em1l1

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// import './styles/Error.css'
-import errorNotFound from '../images/404.jpg'
+import './styles/Error.css'
+// import errorNotFound from '../images/404.jpg'
 // import errorNotFound from '../images/4044.jpg'
 
 function NotFound() {
-  return <img src={errorNotFound}/>
-  // return (
-  //   <React.Fragment>
-  //     <div className="NotFound">
-  //       <Link className="btn btn-primary" to="/home">Home</Link>
-  //     </div>
-  //   </React.Fragment>
-  // )
+  // return <img src={errorNotFound}/>
+  return (
+    <React.Fragment>
+      <div className="NotFound-img">
+        <div className="button-link">
+          <Link className="btn btn-primary button" to="/">Home</Link>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default NotFound;
